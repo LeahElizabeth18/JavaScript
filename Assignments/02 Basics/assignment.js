706981d2cdf19b5ed493f7f8f17a737aae82e265
Leah <leaheliz18@gmail.com>
2024-10-25 15:10:52 -0400

    Completed Assignment 02

--- Assignments/02 Basics/assignment.js
+++ Assignments/02 Basics/assignment.js
@@ -1,1 +1,19 @@
-const task3Element = document.getElementById('task-3');
+const task3Element = document.getElementById("task-3");
+
+function noParam() {
+  alert("No Parameter Function");
+}
+
+function param(name) {
+  alert(name);
+}
+
+function brandNew(str1, str2, str3) {
+  return str1 + str2 + str3;
+}
+
+noParam();
+param("Ooga");
+task3Element.addEventListener("click", noParam);
+
+alert(brandNew("l", "o", "l"));
