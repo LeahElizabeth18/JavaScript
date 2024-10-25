e58933c7382b90b19495857cc627d1bb5f7731e5
Leah <leaheliz18@gmail.com>
2024-10-25 15:04:41 -0400

    Modifie 02

--- Code Along/02 StartingProject/assets/scripts/app.js
+++ Code Along/02 StartingProject/assets/scripts/app.js
@@ -1,5 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-let calculationDescription = "";
-outputResult(currentResult, calculationDescription);
+let calcDescription = "";
+
+addBtn.addEventListener("click", add_nums);
+
+function add_nums() {
+  const result = currentResult + userInput.value;
+  outputResult(currentResult, calcDescription);
+}
