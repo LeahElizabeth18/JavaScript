d00f57ed96f05a6be3d9cc396d5fa1c6452d6524
Leah <leaheliz18@gmail.com>
2024-10-24 23:41:22 -0400

    Completed Assignment 1

---
+++ Assignments/01 Basics/assignment.js
@@ -0,0 +1,10 @@
+
+let fictionalInput = 20;
+let result;
+
+result = fictionalInput + 18;
+result *= 0;
+result += 20;
+result -= 2;
+
+alert(`Input was : ${fictionalInput} and the result was : ${result}`)
